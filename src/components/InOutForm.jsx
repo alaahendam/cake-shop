f9eba7905f2abfeb0f76e0621d691994b7e92eb6
alaahendam <alaa.hendam122@gmail.com>
2023-05-26 12:49:37 +0300

    add visibility button in password

--- src/components/InOutForm.jsx
+++ src/components/InOutForm.jsx
@@ -1,28 +1,56 @@
 import * as React from "react";
+import { useForm, Controller } from "react-hook-form";
 import Box from "@mui/material/Box";
 import InputText from "./InputText";
 import { Button } from "@material-ui/core";
 import { motion } from "framer-motion";
 import styles from "@/styles/login.module.css";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import GoogleIcon from "@mui/icons-material/Google";
 
 function InOutForm({ theme, submitButton }) {
   const AnimatedButton = motion(Button);
+  const { handleSubmit, control, reset } = useForm();
+  const onSubmit = (data) => console.log(data);
   return (
     <Box
       component="form"
       className={styles.inOutForm}
       noValidate
       autoComplete="off"
+      onSubmit={handleSubmit(onSubmit)}
     >
       {submitButton == "Sign Up" ? (
-        <InputText label="First Name" variant="standard" theme={theme} />
+        <InputText
+          required
+          label="First Name"
+          variant="standard"
+          theme={theme}
+          control={control}
+          name="firstName"
+        />
       ) : null}
-      <InputText label="Email" variant="standard" theme={theme} />
-      <InputText label="Password" variant="standard" theme={theme} />
+      <InputText
+        required
+        label="Email"
+        variant="standard"
+        theme={theme}
+        control={control}
+        name="email"
+      />
+      <InputText
+        required
+        label="Password"
+        variant="standard"
+        theme={theme}
+        type="password"
+        control={control}
+        name="password"
+      />
       <motion.div />
       <AnimatedButton
         whileHover={{ scale: 1.05 }}
-        whileTap={{ scale: 0.9 }}
+        whileTap={{ scale: 0.99 }}
         sx={{
           color: theme.palette.secondary.contrastText,
           backgroundColor: theme.palette.secondary.main,
@@ -30,9 +58,25 @@ function InOutForm({ theme, submitButton }) {
             backgroundColor: theme.palette.secondary.main,
           },
         }}
+        type="submit"
       >
         {submitButton}
       </AnimatedButton>
+      <div
+        style={{
+          width: "100%",
+          display: "flex",
+          justifyContent: "center",
+          color: theme.palette.primary.main,
+        }}
+      >
+        <motion.div whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.9 }}>
+          <FacebookIcon sx={{ cursor: "pointer", fontSize: "35px" }} />
+        </motion.div>
+        <motion.div whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.9 }}>
+          <GoogleIcon sx={{ cursor: "pointer", fontSize: "35px" }} />
+        </motion.div>
+      </div>
     </Box>
   );
 }
--- src/components/InputText.jsx
+++ src/components/InputText.jsx
@@ -1,30 +1,73 @@
 import TextField from "@mui/material/TextField";
-const InputText = ({ label, variant, theme, error }) => {
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
+import { useState } from "react";
+import { useForm, Controller } from "react-hook-form";
+
+const InputText = ({ label, variant, theme, error, type, control, name }) => {
+  const [visibility, setVisibility] = useState(true);
+
   return (
-    <TextField
-      id="standard-basic"
-      label={label}
-      variant={variant}
-      InputLabelProps={{
-        sx: {
-          color: theme.palette.primary.main,
-        },
-      }}
-      sx={{
-        width: "100%",
-        ".css-1x51dt5-MuiInputBase-input-MuiInput-input": {
-          color: theme.palette.primary.main,
-        },
-        borderBottom: `1px solid ${theme.palette.primary.main}`,
-        ".css-a6v7lc-MuiInputBase-root-MuiInput-root:hover:not(.Mui-disabled, .Mui-error):before ":
-          {
-            borderBottom: `1px solid ${theme.palette.primary.main}`,
-          },
-        ".css-a6v7lc-MuiInputBase-root-MuiInput-root:before": {
-          borderBottom: `1px solid ${theme.palette.primary.main}`,
-        },
-      }}
-    />
+    <div style={{ position: "relative" }}>
+      <Controller
+        name={name}
+        control={control}
+        defaultValue=""
+        render={({ field }) => (
+          <TextField
+            {...field}
+            id={name}
+            label={label}
+            type={type && visibility ? type : "text"}
+            variant={variant}
+            InputLabelProps={{
+              sx: {
+                color: theme.palette.primary.main,
+              },
+            }}
+            sx={{
+              width: "100%",
+              ".css-1x51dt5-MuiInputBase-input-MuiInput-input": {
+                color: theme.palette.primary.main,
+              },
+              borderBottom: `1px solid ${theme.palette.primary.main}`,
+              ".css-a6v7lc-MuiInputBase-root-MuiInput-root:hover:not(.Mui-disabled, .Mui-error):before":
+                {
+                  borderBottom: `1px solid ${theme.palette.primary.main}`,
+                },
+              ".css-a6v7lc-MuiInputBase-root-MuiInput-root:before": {
+                borderBottom: `1px solid ${theme.palette.primary.main}`,
+              },
+            }}
+          />
+        )}
+      />
+      {type === "password" &&
+        (visibility ? (
+          <VisibilityIcon
+            sx={{
+              position: "absolute",
+              right: "10px",
+              top: "20px",
+              cursor: "pointer",
+              color: theme.palette.primary.main,
+            }}
+            onClick={() => setVisibility(!visibility)}
+          />
+        ) : (
+          <VisibilityOffIcon
+            sx={{
+              position: "absolute",
+              right: "10px",
+              top: "20px",
+              cursor: "pointer",
+              color: theme.palette.primary.main,
+            }}
+            onClick={() => setVisibility(!visibility)}
+          />
+        ))}
+    </div>
   );
 };
+
 export default InputText;
