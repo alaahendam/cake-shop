d37c3978d3ded517a8c209a0af0f908a9269cedb
alaahendam <alaa.hendam122@gmail.com>
2023-06-06 19:31:25 +0300

    fix vercel problem

--- src/components/numberDisplay.jsx
+++ src/components/numberDisplay.jsx
@@ -1,9 +1,19 @@
+import React, { useEffect, useRef } from "react";
 import dynamic from "next/dynamic";
+
 const CountUp = dynamic(() => import("react-countup"), {
   ssr: false, // Disable server-side rendering
 });
 
 const NumberDisplay = ({ targetNumber, duration }) => {
+  const countUpRef = useRef(null);
+
+  useEffect(() => {
+    if (countUpRef.current) {
+      countUpRef.current.start();
+    }
+  }, [targetNumber]);
+
   return (
     <CountUp start={0} end={targetNumber} duration={duration} separator=".">
       {({ countUpRef }) => <span ref={countUpRef} />}
