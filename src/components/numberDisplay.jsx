2574e2fc4dac5aebf30413272a553b166399b513
alaahendam <alaa.hendam122@gmail.com>
2023-06-06 19:50:26 +0300

    add use-count-up package

--- src/components/numberDisplay.jsx
+++ src/components/numberDisplay.jsx
@@ -1,22 +1,15 @@
-import React, { useEffect, useRef } from "react";
-import dynamic from "next/dynamic";
-
-const CountUp = dynamic(() => import("react-countup"), {
-  ssr: true, // Enable server-side rendering
-});
+import { CountUp } from "use-count-up";
 
 const NumberDisplay = ({ targetNumber, duration }) => {
-  const countUpRef = useRef(null);
-
-  useEffect(() => {
-    if (countUpRef.current) {
-      countUpRef.current.start();
-    }
-  }, [targetNumber]);
-
   return (
-    <CountUp start={0} end={targetNumber} duration={duration} separator=".">
-      {({ countUpRef }) => <span ref={countUpRef} />}
+    <CountUp
+      isCounting
+      start={0}
+      end={targetNumber}
+      duration={duration}
+      thousandsSeparator="."
+    >
+      {({ value, reset }) => <span>{value}</span>}
     </CountUp>
   );
 };
