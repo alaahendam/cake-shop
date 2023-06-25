2e85870665906ddac958de80403bfa0b1ec0c81f
alaahendam <alaa.hendam122@gmail.com>
2023-06-25 14:08:02 +0300

    add checkout page

--- src/components/shopping/checkout.jsx
+++ src/components/shopping/checkout.jsx
@@ -2,11 +2,85 @@ import React from "react";
 import { motion } from "framer-motion";
 import { useDispatch } from "react-redux";
 import { changeActiveProcess } from "@/redux/features/activeProcess";
+import CloseIcon from "@mui/icons-material/Close";
+import { IconButton } from "@mui/material";
+import Image from "next/image";
 const Checkout = () => {
   const dispatch = useDispatch();
+  const data = [
+    {
+      name: "chesse cake",
+      flavour: "chesse",
+      img: "cheeseCake.jpg",
+      price: "10 AMD",
+      quantity: 2,
+      total: "20 AMD",
+    },
+    {
+      name: "chesse cake",
+      flavour: "chesse",
+      img: "cheeseCake.jpg",
+      price: "10 AMD",
+      quantity: 2,
+      total: "20 AMD",
+    },
+    {
+      name: "chesse cake",
+      flavour: "chesse",
+      img: "cheeseCake.jpg",
+      price: "10 AMD",
+      quantity: 2,
+      total: "20 AMD",
+    },
+    {
+      name: "chesse cake",
+      flavour: "chesse",
+      img: "cheeseCake.jpg",
+      price: "10 AMD",
+      quantity: 2,
+      total: "20 AMD",
+    },
+  ];
   return (
-    <div className="flex justify-center items-center">
-      <h1 className="text-2xl text-pink-700 font-semibold">Checkout</h1>
+    <div className="flex justify-center items-center flex-col py-5">
+      <h1 className="text-2xl text-pink-700 font-semibold pb-3">Checkout</h1>
+      <div
+        className="flex justify-center w-full flex-wrap px-5 md:px-28"
+        dir="rtl"
+      >
+        <div
+          className="bg-[#ffdfbf] basis-full md:basis-1/3 rounded-md px-2 py-4"
+          dir="ltr"
+        >
+          <h1 className="text-1xl text-[#ba9169] font-semibold">Your Order</h1>
+          <div>
+            {data.map((item) => (
+              <div className="flex items-center my-2">
+                <Image
+                  priority
+                  src={`/images/${item?.img ? item.img : "cheeseCake.jpg"}`}
+                  alt="Example Image"
+                  width={300}
+                  height={400}
+                  className="w-1/4 h-20 rounded-md "
+                />
+                <div className="w-2/3 px-2">
+                  <h2>{item.name}</h2>
+                  <p>{item.flavour}</p>
+                  <div className="flex justify-between">
+                    <p>{item.quantity}</p>
+                    <p>{item.price}</p>
+                  </div>
+                </div>
+                <IconButton>
+                  <CloseIcon />
+                </IconButton>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="bg-gray-700 basis-full md:basis-2/3 h-52"></div>
+      </div>
       <div className="flex justify-center items-center py-5">
         <motion.button
           whileHover={{ scale: 1.05 }}
